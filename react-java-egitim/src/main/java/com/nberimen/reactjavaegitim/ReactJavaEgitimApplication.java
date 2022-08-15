570276e80065a7fafdc5dfbe542a630799f5c69e
Necati Berimen <berimen66@gmail.com>
2022-08-15 11:15:29 +0300

    first commit

---
+++ react-java-egitim/src/main/java/com/nberimen/reactjavaegitim/ReactJavaEgitimApplication.java
@@ -0,0 +1,13 @@
+package com.nberimen.reactjavaegitim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactJavaEgitimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactJavaEgitimApplication.class, args);
+	}
+
+}
---
+++ react-java-egitim/src/test/java/com/nberimen/reactjavaegitim/ReactJavaEgitimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nberimen.reactjavaegitim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactJavaEgitimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
