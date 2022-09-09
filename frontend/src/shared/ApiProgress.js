148e6b90c853ddca6e9a473789f1d52ac9f75763
nberimen <berimen66@gmail.com>
2022-09-09 16:19:53 +0300

    Create ApiProgress

---
+++ frontend/src/shared/ApiProgress.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useApiProgress = (apiPath) => {
+  const [pendingApiCall, setPendingApiCall] = useState(false);
+
+  useEffect(() => {
+    let requestInterceptor, responseInterceptor;
+    const updateApiCallFor = (url, inProgress) => {
+      if (url === apiPath) {
+        setPendingApiCall(inProgress);
+      }
+    };
+    const registerInterceptors = () => {
+      requestInterceptor = axios.interceptors.request.use((request) => {
+        const { url } = request;
+        updateApiCallFor(url, true);
+        return request;
+      });
+      responseInterceptor = axios.interceptors.response.use(
+        (response) => {
+          const { url } = response.config;
+          updateApiCallFor(url, false);
+          return response;
+        },
+        (error) => {
+          const { url } = error.config;
+          updateApiCallFor(url, false);
+          throw error;
+        }
+      );
+    };
+    const unregisterInterceptors = () => {
+      axios.interceptors.request.eject(requestInterceptor);
+      axios.interceptors.response.eject(responseInterceptor);
+    };
+    registerInterceptors();
+    return function unmount() {
+      unregisterInterceptors();
+    };
+  }, [apiPath]);
+  return pendingApiCall;
+};
